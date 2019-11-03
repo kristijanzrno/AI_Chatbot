369e2f8373f897ac2145a23540ac2019ccf08085
kristijanzrno <superkiki.info.android@gmail.com>
2019-11-03 20:30:46 +0000

    created UI files

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,21 @@
 import aiml
+from flask import Flask, render_template, request
 
-print("test")
+kernel = aiml.Kernel()
+kernel.setTextEncoding(None)
+kernel.bootstrap(learnFiles="rules.xml")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+   return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    textInput = request.args.get('msg')
+    print(textInput)
+    return str(kernel.respond(textInput))
+
+if __name__ == "__main__":
+    app.run()
