677458fd1e7a3d0ce417867f315f31ce4f33f52d
n0763258 <kristijan.zrno2017@my.ntu.ac.uk>
2019-11-04 02:33:11 +0000

    Working on a similarity-based component

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,20 @@
 import aiml
+import nltk
+import string
 from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = Flask(__name__)
+
+sent_tokens = []
+word_tokens = []
+lemmer = nltk.stem.WordNetLemmatizer()
 
 kernel = aiml.Kernel()
 kernel.setTextEncoding(None)
 kernel.bootstrap(learnFiles='rules.xml')
 
-app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -13,13 +22,13 @@ def home():
 
 @app.route('/get')
 def get_bot_response():
-    textInput = request.args.get('msg')
-    return str(processQuery(textInput))
+    text_input = request.args.get('msg')
+    return str(process_query(text_input))
 
-def processQuery(userInput):
-    responseAgent = 'aiml'
-    if responseAgent == 'aiml':
-        answer = kernel.respond(userInput)
+def process_query(user_input):
+    response_agent = 'aiml'
+    if response_agent == 'aiml':
+        answer = kernel.respond(user_input)
     #post-process the answer for commands
     if answer[0] == '#':
         params = answer[1:].split('$')
@@ -31,10 +40,45 @@ def processQuery(userInput):
         elif cmd == 2:
             return ""
         elif cmd == 99:
-            return "I did not get that, please try again."
+            #similarity component
+            return check_similarity(user_input)
     else:
         return answer
 
+def load_data():
+    data = open('data.txt', 'r')
+    raw = data.read().lower()
+    nltk.download('punkt') # first-time use only
+    nltk.download('wordnet') # first-time use only
+    sent_tokens = nltk.sent_tokenize(raw)
+    word_tokens = nltk.word_tokenize(raw)
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+def check_similarity(user_input):
+    result = ''
+    sent_tokens.append(user_input)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfdif = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfdif[-1], tfdif)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfdif = flat[-2]
+    if(req_tfdif == 0):
+        result = result + "I'm sorry, I couldnt understand you."
+    else:
+        result = result + sent_tokens[idx]
+    return result
+
+load_data()
 
 if __name__ == '__main__':
     app.run()
