6130daa6025442bb7a9c52442b5e7d179b86f5b4
kristijanzrno <superkiki.info.android@gmail.com>
2019-11-04 00:57:14 +0000

    added process query function, html edits

--- chatbot.py
+++ chatbot.py
@@ -3,19 +3,38 @@ from flask import Flask, render_template, request
 
 kernel = aiml.Kernel()
 kernel.setTextEncoding(None)
-kernel.bootstrap(learnFiles="rules.xml")
+kernel.bootstrap(learnFiles='rules.xml')
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-   return render_template("index.html")
+   return render_template('index.html')
 
-@app.route("/get")
+@app.route('/get')
 def get_bot_response():
     textInput = request.args.get('msg')
-    print(textInput)
-    return str(kernel.respond(textInput))
+    return str(processQuery(textInput))
 
-if __name__ == "__main__":
+def processQuery(userInput):
+    responseAgent = 'aiml'
+    if responseAgent == 'aiml':
+        answer = kernel.respond(userInput)
+    #post-process the answer for commands
+    if answer[0] == '#':
+        params = answer[1:].split('$')
+        cmd = int(params[0])
+        if cmd == 0:
+            return params[1]
+        elif cmd == 1:
+            return "wikipedia article"
+        elif cmd == 2:
+            return ""
+        elif cmd == 99:
+            return "I did not get that, please try again."
+    else:
+        return answer
+
+
+if __name__ == '__main__':
     app.run()
